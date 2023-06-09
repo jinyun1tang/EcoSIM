7d8156d45c728f473a9d9fd286407c74827dc5d5
Jinyun Tang <jinyuntang@lbl.gov>
2023-06-09 14:56:44 -0700

    separated the snow hydrothermal code from soil
    This prepares the building of a standalone surface energy balance
    model.

--- f90src/main/InitEcoSIM.F90
+++ f90src/main/InitEcoSIM.F90
@@ -13,18 +13,18 @@ module InitEcoSIM
 
   subroutine InitModules(NOMicrobeGuilds)
   use PlantDisturbsMod     , only : InitPlantDisturbance
-  use UptakesMod           , only : InitUptake  
-  use WatsubMod            , only : initWatsub  
+  use UptakesMod           , only : InitUptake
+  use WatsubMod            , only : initWatsub
   use NitrosMod            , only : InitNitro
   use RedistMod            , only : InitRedist
   use ErosionMod           , only : InitErosion
   use Hour1Mod             , only : InitHour1
-  use HistDataType         , only : hist_ecosim  
+  use HistDataType         , only : hist_ecosim
   use InitAllocMod         , only : InitAlloc
-  use UnitMod              , only : units  
-  use GridConsts  
+  use UnitMod              , only : units
+  use GridConsts
   implicit  none
-  
+
   integer                 , intent(in) :: NOMicrobeGuilds   !number of microbial guilds per group
 
 
@@ -47,7 +47,7 @@ module InitEcoSIM
   call InitHour1(micpar%n_litrsfk)
 
   call InitModules2
-    
+
   call hist_ecosim%Init(bounds)
 
   end subroutine InitModules
