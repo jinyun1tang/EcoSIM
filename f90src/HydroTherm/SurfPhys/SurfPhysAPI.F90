1ccb0658f4b5a1ed2fb69315027b995ae145fad0
Jinyun Tang <jinyuntang@lbl.gov>
2023-06-19 16:36:19 -0700

    finished the first part of separting surface physics scheme
    Also fixed some bugs in resistance calculation, which lead to
    differences in results.

---
+++ f90src/HydroTherm/SurfPhys/SurfPhysAPI.F90
@@ -0,0 +1,12 @@
+module SurfPhysAPI
+  use data_kind_mod, only : r8 => DAT_KIND_R8
+
+implicit none
+  private
+  character(len=*), parameter :: mod_filename=__FILE__
+
+
+contains
+
+
+end module SurfPhysAPI
