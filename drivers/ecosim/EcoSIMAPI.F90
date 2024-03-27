15a168b4069362e620a7d17d60aea82ce08516fb
Jinyun Tang <jinyuntang@lbl.gov>
2024-03-26 21:40:13 -0700

    updated plant flag for ecosim driver

--- drivers/ecosim/EcoSIMAPI.F90
+++ drivers/ecosim/EcoSIMAPI.F90
@@ -463,7 +463,7 @@ subroutine regressiontest(nmfile,case_name, NX, NY)
     call regression%OpenOutput()
 
     do NZ=1,NP(NY,NX)
-      IF(IsPlantActive_pft(NZ,NY,NX).EQ.iPlantIsActive)THEN
+      IF(IsPlantActive_pft(NZ,NY,NX).EQ.iActive)THEN
 
         category = 'flux'
         name = 'NH4_UPTK (g m^-3 h^-1)'
